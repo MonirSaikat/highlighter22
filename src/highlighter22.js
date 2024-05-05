2c1f3ce02488343ef88f466595e124f5f7353b86
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-05-05 22:37:52 +0600

    data attr & filter

--- src/highlighter22.js
+++ src/highlighter22.js
@@ -9,26 +9,43 @@
       color: 'black',
       backgroundColor: '#FFFF00',
       padding: 0,
-      targets: []
+      targets: [],
+      filter: false,
     }, options);
 
     function markStyle() {
       return `color: ${settings.color}; background-color: ${settings.backgroundColor}; padding: ${settings.padding}px;`;
     }
 
-
     return this.each(function () {
       var $input = $(this);
 
       $input.on('input', function () {
         var inputValue = $input.val().toString().trim();
-        var lowerCaseValue;
+        var lowerCaseValue = inputValue.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
-        lowerCaseValue = inputValue.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        // filter feature
+        if (settings.filter) {
+          $('[data-filter]').each(function (item) {
+            const filterableContent = $(this)
+              .text()
+              .toString()
+              .replace('\n', '')
+              .replace('\t', '')
+              .toLowerCase()
+              .trim();
 
-        if (settings.targets.length > 0) {
-          $.each(settings.targets, function (index, targetEl) {
+            $(this).hide();
 
+            if (filterableContent.includes(lowerCaseValue)) {
+              $(this).show();
+            }
+          });
+        }
+
+        const targets = $('[data-highlighter-22] [data-key]')
+        if (targets.length > 0) {
+          $.each(targets, function (index, targetEl) {
             $(targetEl).each(function (_index, target) {
               var highlightedText = $(target).text().replace(new RegExp(lowerCaseValue, 'gi'), function (match) {
                 return `<mark style="${markStyle()}">${match}</mark>`;
